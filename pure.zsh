75f338cd5aff3a058ee3078c809371794cb55ad0
Paolo Jovon <paolo.jovon@gmail.com>
2020-06-03 16:05:38 +0100

    Add exit code to error prompt

--- pure.zsh
+++ pure.zsh
@@ -800,8 +800,8 @@ prompt_pure_setup() {
 	# If a virtualenv is activated, display it in grey.
 	PROMPT='%(12V.%F{$prompt_pure_colors[virtualenv]}%12v%f .)'
 
-	# Prompt turns red if the previous command didn't exit with 0.
-	local prompt_indicator='%(?.%F{$prompt_pure_colors[prompt:success]}.%F{$prompt_pure_colors[prompt:error]})${prompt_pure_state[prompt]}%f '
+	# Prompt turns red and shows status code if the previous command didn't exit with 0.
+	local prompt_indicator='%(?.%F{$prompt_pure_colors[prompt:success]}.%F{$prompt_pure_colors[prompt:error]}%? )${prompt_pure_state[prompt]}%f '
 	PROMPT+=$prompt_indicator
 
 	# Indicate continuation prompt by … and use a darker color for it.
